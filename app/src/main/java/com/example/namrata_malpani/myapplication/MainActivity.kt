039c962892cdc7e6012aaff310f46f95ea79d6fe
Namrata92 <namratamalpani12@gmail.com>
2018-05-25 12:19:27 +0530

    Initial project added

---
+++ app/src/main/java/com/example/namrata_malpani/myapplication/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.namrata_malpani.myapplication
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun add(){
+        val i = 1 +1
+
+    }
+}
